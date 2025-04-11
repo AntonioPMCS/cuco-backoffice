a15696b646745fa3a91d0335e26ad0c36ac8ce6d
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-04-11 11:44:27 -0300

    added softi9 logo

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,6 +5,7 @@ import '../styles/Layout.css';
 import eu_logo from "../assets/uniao_europeia_logo.png";
 import pt_logo from "../assets/republica_portuguesa_logo.png"
 import prr_logo from "../assets/prr_logo.jfif"
+import softi9_logo from "../assets/softi9_logo.png"
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -18,6 +19,7 @@ const Layout = ({ }: LayoutProps) => {
         <Outlet />
       </div>
       <div className="footer flex items-center justify-between mt-10 space-x-10">
+        <img src={softi9_logo} className="h-10 max-h-12 object-contain" alt="SoftI9 logo" />
         <img src={prr_logo} className="h-10 max-h-12 object-contain" alt="PRR logo" />
         <img src={pt_logo} className="h-10 max-h-12 object-contain" alt="PT logo" />
         <img src={eu_logo} className="h-10 max-h-12 object-contain" alt="EU logo" />
